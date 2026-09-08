8fc58c191c2cb41f49894449c61c5389708030c3
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-08 15:39:09 +0100

    solved assignment-09

--- day01/exercices/exercice-09.js
+++ day01/exercices/exercice-09.js
@@ -18,3 +18,13 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let estAdmin = false;
+let estModerateur = true;
+let motDEPasseValide = true;
+
+if (motDEPasseValide && (estAdmin || estModerateur)) {
+    console.log("Accès Accordé");
+} else {
+    console.log("incorrect");
+}
