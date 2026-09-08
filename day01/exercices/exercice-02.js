a7b5f0fb6e45c56bb1aef4702a10bff864445224
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-08 14:40:11 +0100

    solved assignment-01

--- day01/exercices/exercice-02.js
+++ day01/exercices/exercice-02.js
@@ -15,3 +15,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log("Bonjour, je m'appelle", firstName, lastName);
