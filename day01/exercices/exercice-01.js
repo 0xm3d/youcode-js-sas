5524b43f54eff42d382746b163398a39ae5df872
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-08 12:28:41 +0100

    assignment 01 solved

--- day01/exercices/exercice-01.js
+++ day01/exercices/exercice-01.js
@@ -18,3 +18,11 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution iccd
+
+
+const PI = 3.14;
+let rayon = 5;
+let perimeter;
+perimeter = 2 * PI * rayon;
+
+console.log(perimeter);
