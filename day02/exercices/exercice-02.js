9d63cb431a2cca6985f65d55a8f465e5b57b0aa0
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:11:49 +0100

    assignment-02 is done

--- day02/exercices/exercice-02.js
+++ day02/exercices/exercice-02.js
@@ -15,3 +15,11 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+for (let a = 0; a <= 20; a++) {
+    if (a % 2 == 0) {
+        console.log(a);
+    }else {
+        continue
+    }
+}
