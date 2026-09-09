f7eb095b2c6d66409f9a7fd83f39d8e6c07ea0c4
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:13:44 +0100

    assignment-11 is done

--- day02/exercices/exercice-12.js
+++ day02/exercices/exercice-12.js
@@ -3,7 +3,8 @@
  * JOUR 02 · EXERCICE 12 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
  * NOMBRE PREMIER
  * ─────────────────────────────────────────────────────────────
- *
+ * * Un nombre est premier s'il n'est divisible que par 1 et par lui-même. Prenez let nb = 29. Bouclez de 2 jusqu'à 28 pour voir si un nombre le divise. Si ce n'est pas le cas, affichez "29 est un nombre premier".
+
  * 🎯 MISSION
  * Un nombre est premier s'il n'est divisible que par 1 et par lui-même. Prenez let nb = 29. Bouclez de 2 jusqu'à 28 pour voir si un nombre le divise. Si ce n'est pas le cas, affichez "29 est un nombre premier".
  *
@@ -15,3 +16,11 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let nombre = 29;
+for (let a = 2; a <= 29; a++){
+    if (nombre % 1 == 0 && nombre % a == 0)
+        console.log(`${nombre} est un nombre premier`);
+}
+
+
