c32859d3cc0c472ad436cd1cc5e9c7ec2d85cbba
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:13:19 +0100

    assignment-11 is done

--- day02/exercices/exercice-11.js
+++ day02/exercices/exercice-11.js
@@ -18,3 +18,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let a = 2, result = 1;
+for (let i = 0; i < 10; i++) {
+    result *= a;
+}
+console.log(result);
