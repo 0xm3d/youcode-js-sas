59e02fcade9861c94c5001454915d975b4617baf
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:11:38 +0100

    assignment-01 is done

--- day02/exercices/exercice-01.js
+++ day02/exercices/exercice-01.js
@@ -15,3 +15,8 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+for (let a = 10; a >= 0; a--) {
+    console.log(a);
+}
+console.log("Décollage !");
