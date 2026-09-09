48b724df26556d06031c31a93251efedbedf34f4
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:11:56 +0100

    assignment-03 is done

--- day02/exercices/exercice-03.js
+++ day02/exercices/exercice-03.js
@@ -15,3 +15,8 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+for (let a = 1; a <= 10; a++) {
+    let num = 7;
+    console.log(`${num} X ${a} = ${a * num}`);
+}
