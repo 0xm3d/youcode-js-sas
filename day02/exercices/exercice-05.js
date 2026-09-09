77e16a4167846d817ef59cb887ecad397151a0d7
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:12:19 +0100

    assignment-05 is done

--- day02/exercices/exercice-05.js
+++ day02/exercices/exercice-05.js
@@ -15,3 +15,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let motDePasse = "faux";
+do {
+    motDePasse = "secret";
+    console.log("Veuillez entrer le mot de passe");
+}while (motDePasse == "faux");
