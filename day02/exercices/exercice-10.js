06d6c5a353ee005f56f50f4c6e8cfbdcbbd8fd5f
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:13:09 +0100

    assignment-10 is done

--- day02/exercices/exercice-10.js
+++ day02/exercices/exercice-10.js
@@ -18,3 +18,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let factorielle = 6;
+for (let a = 5; a > 0; a--) {
+    factorielle *= a;
+}
+console.log(factorielle);
