e4c1d4f281c7a72e7d79cb3f57cf86dc985a44c0
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:12:45 +0100

    assignment-08 is done

--- day02/exercices/exercice-08.js
+++ day02/exercices/exercice-08.js
@@ -15,3 +15,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+for (let a = 1; a <= 20; a++) {
+    if (a % 3 == 0) 
+        continue
+    console.log(a);
+}
