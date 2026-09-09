4737a29b5e787565fef1a2d9bd9a1ddc4fb2b9dc
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-10 00:51:39 +0100

    Assignment-16 is done

--- day02/exercices/exercice-16.js
+++ day02/exercices/exercice-16.js
@@ -21,3 +21,14 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+for(let a = 1; a <= 5; a++) {
+    let line = "";
+    for (let c = 1; c <=  5 - a; c++) {
+        line += " ";
+    }
+    for(let b = 1; b <= a * 2 -1 ; b++) {
+        line += "*";
+    }
+    console.log(line);
+} 
