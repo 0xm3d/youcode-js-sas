f0c4c197189ab332a899f41c49ee8149ed544387
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-09 20:12:06 +0100

    assignment-04 is done

--- day02/exercices/exercice-04.js
+++ day02/exercices/exercice-04.js
@@ -18,3 +18,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let total = 0;
+for (let i = 1; i <= 50; i++) {
+    total += i;
+}
+console.log(total);
