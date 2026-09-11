f6f762c716abe1da9097a9581b90f905d96c34e6
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-11 07:14:06 +0100

    added exercice-12

--- day03/exercices/exercice-12.js
+++ day03/exercices/exercice-12.js
@@ -14,4 +14,13 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function multiplier(a, b) {
+    return a * b
+}
+
+function executerOperation(a, b, multiplier) {
+    return multiplier(a, b)
+}
+
+console.log(executerOperation(12, 2, multiplier))
