a3b2d647cb9a7830ff8b336734c8a3cf91594096
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-12 16:28:08 +0100

    exercice-01 is done

--- day04/exercices/exercice-01.js
+++ day04/exercices/exercice-01.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+let colors = ["blue", "gray", "black", "purple", "white"]
+
+console.log(colors[0], colors[2])
