94d249fda047f93e6fbd4c747e31b0b87d65ca47
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-12 21:55:45 +0100

    exercice-11 is done

--- day04/exercices/exercice-11.js
+++ day04/exercices/exercice-11.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+const grandTableau = []
+for (let i = 1; i <= 100; i++) {
+    grandTableau.push(i)
+}
+console.log(grandTableau.slice(0, 10))
