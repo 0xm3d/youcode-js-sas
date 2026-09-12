79845129ea486bbec456071995b446a07ed377d1
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-12 20:34:41 +0100

    exercice-03 is done

--- day04/exercices/exercice-03.js
+++ day04/exercices/exercice-03.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const invites = ["Yassine", "Sara", "Amine"]
+invites.pop(invites.length - 1)
+console.log(invites)
