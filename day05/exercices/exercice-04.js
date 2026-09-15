a2f36fc22d93e29612bb136b16cc8ae46b5e8d0e
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-15 04:46:53 +0100

    done

--- day05/exercices/exercice-04.js
+++ day05/exercices/exercice-04.js
@@ -14,4 +14,10 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let email = "m3dbouhadi69gmail.com"
+let char = '@'
+if (email.includes(char)) 
+    console.log("your eamil is correct")
+else
+    console.log("your email doesn't contain the '@' symbole!!!")
