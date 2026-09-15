5f55bb6c9d80aa1428fb6810720b11ee0841da04
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-15 09:09:26 +0100

    done

--- day05/exercices/exercice-12.js
+++ day05/exercices/exercice-12.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let phrase = "Bonjour le monde"
+let arr = phrase.split(" ")
+console.log(`${arr[2]} ${arr[1]} ${arr[0]}`)
