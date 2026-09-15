0c273657d8088a7147b76bd053680bd9987aed8a
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-15 22:02:04 +0100

    ex-12 is done

--- day06/exercices/exercice-12.js
+++ day06/exercices/exercice-12.js
@@ -14,4 +14,13 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const compteBancaire = {
+    solde: 999,
+    deposer(montant) {
+        this.solde += montant
+    }
+}
+compteBancaire.deposer(1)
+
+console.log(compteBancaire.solde)
