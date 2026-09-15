f2424a61ce37f9f1a4f83721899d8861d33cca9d
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-15 09:09:51 +0100

    done

--- day05/exercices/exercice-15.js
+++ day05/exercices/exercice-15.js
@@ -15,4 +15,21 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let word1 = "chien"
+let word2 = "niche"
+
+function isEqual(word1, word2) {
+    const arr1 = word1.split("")
+    const arr2 = word2.split("")
+
+    arr1.sort()
+    arr2.sort()
+
+    for(let i = 0; i < word1.length; i++) {
+        if (arr1[i] !== arr2[i])
+            return `the two words don't have the same letters`
+    }
+    return `"${word1}" and "${word2}" have the same letters and the same number of repeated letters`
+}
+console.log(isEqual(word1, word2))
