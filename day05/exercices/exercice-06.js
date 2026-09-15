3b485936147ee92f0bd646cc5d024d26c9580b17
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-15 04:47:21 +0100

    done

--- day05/exercices/exercice-06.js
+++ day05/exercices/exercice-06.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let cities = "Safi,Youssoufia,Nador,Casablanca"
+let array = cities.split(",")
+console.log(array)
+console.table(array)
