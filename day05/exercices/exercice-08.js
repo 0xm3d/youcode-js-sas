2e08545d7867e638b360d7305ae697c9b8ab5508
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-15 04:47:48 +0100

    done

--- day05/exercices/exercice-08.js
+++ day05/exercices/exercice-08.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let mot = "yOUcoDE"
+let result = mot[0].toUpperCase() + mot.slice(1).toLocaleLowerCase()
+console.log(result)
+
