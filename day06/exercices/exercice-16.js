0facab210ee172dbab9c56ed5406a337e393094f
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-16 07:47:12 +0100

    done

--- day06/exercices/exercice-16.js
+++ day06/exercices/exercice-16.js
@@ -14,4 +14,13 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const entreprise = {
+    nom: "TechCorp",
+    employes: [
+        { prenom: "Med" },
+        { prenom: "Amine" },
+        { prenom: "Yassine" }
+    ]
+}
+console.log(entreprise.employes[1].prenom)
